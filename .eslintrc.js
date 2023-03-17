7da8ec7ff583b55cafd9bcfbd93fe017b6c7ff71
Josh Justice <me@codingitwrong.com>
2023-03-17 06:24:33 -0400

    Configure linting and formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,36 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community'],
+  plugins: [
+    'import',
+    'detox',
+    
+  ],
+  
+  rules: {
+    'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
+    'no-duplicate-imports': 'error',
+    quotes: ['error', 'single', {avoidEscape: true}], // single quote unless using interpolation
+    'react/jsx-uses-react': 'off',
+    'react/no-unstable-nested-components': ['warn', {allowAsProps: true}], // allow function props that return components
+    'react/react-in-jsx-scope': 'off',
+    'sort-imports': [
+      'warn',
+      {ignoreDeclarationSort: true, ignoreMemberSort: false},
+    ], // alphabetize named imports - https://eslint.org/docs/rules/sort-imports
+  },
+  overrides: [
+  {
+      files: ['e2e/**/*.e2e.js'],
+      env: {
+        'detox/detox': true,
+        jest: true,
+        'jest/globals': true,
+      },
+    },
+  {
+      files: ['src/**/*.spec.js'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
 };
