a8b88460fbc6def25f5f92ee66bbe3d6aad8b873
Josh Justice <me@codingitwrong.com>
2023-03-29 19:40:44 -0400

    Make buttons work consistently on Android

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import {useState} from 'react';
-import {Button, SafeAreaView, StatusBar} from 'react-native';
+import {
+  Pressable,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+} from 'react-native';
 import Lecture2 from './lecture2';
 import Lecture3 from './lecture3';
 import Lecture4 from './lecture4';
@@ -72,3 +78,22 @@ function Exercise() {
 function BackButton({setLectureShown}) {
   return <Button title="Back" onPress={() => setLectureShown(null)} />;
 }
+
+function Button({title, onPress}) {
+  return (
+    <Pressable onPress={onPress} style={styles.button}>
+      <Text>{title}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    borderColor: 'gray',
+    borderRadius: 8,
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 8,
+    alignItems: 'center',
+  },
+});
