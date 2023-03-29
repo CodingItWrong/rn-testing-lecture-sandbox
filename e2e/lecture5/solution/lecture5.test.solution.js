6e92e6309ffcecf4f2a5743a41b5f260dc49cb26
Josh Justice <me@codingitwrong.com>
2023-03-29 19:11:24 -0400

    Update lecutre 5 solution

--- e2e/lecture5/solution/lecture5.test.solution.js
+++ e2e/lecture5/solution/lecture5.test.solution.js
@@ -9,28 +9,20 @@ describe('Lecture 5', () => {
   });
 
   it('detects text', async () => {
-    await expect(element(by.text('Hello, Lecture 5!'))).toBeVisible();
+    await expect(element(by.label('Hello, Lecture 5!'))).toBeVisible();
     await expect(element(by.id('welcome'))).toBeVisible();
     await expect(element(by.id('welcome'))).toHaveText('Hello, Lecture 5!');
   });
 
   it('allows tapping buttons', async () => {
     await element(by.id('mybutton')).tap();
-    // await element(by.text('Tap Me')).tap();
-    // await element(by.label('Tap Me')).tap();
     await expect(element(by.text('You tapped the button!'))).toBeVisible();
   });
 
   it('allows typing into text fields', async () => {
     await element(by.id('mytextinput')).typeText('I typed this');
-    await expect(element(by.id('mytextinput'))).toHaveValue('I typed this');
     await expect(element(by.text('Entered text: I typed this'))).toBeVisible();
-
+    await expect(element(by.id('mytextinput'))).toHaveValue('I typed this');
     await element(by.id('mytextinput')).replaceText('Something else');
-    await expect(
-      element(by.text('Entered text: Something else')),
-    ).toBeVisible();
-
-    await element(by.id('mytextinput')).clearText();
   });
 });
