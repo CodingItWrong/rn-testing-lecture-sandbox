014e59a400160557f723fbeb3f36696a82f59ceb
Josh Justice <me@codingitwrong.com>
2023-05-09 06:33:05 -0400

    Remove starter tests for lecture 5

--- e2e/lecture5/lecture5.test.js
+++ e2e/lecture5/lecture5.test.js
@@ -7,16 +7,4 @@ describe('Lecture 5', () => {
     await device.reloadReactNative();
     await element(by.text('Lecture 5')).tap();
   });
-
-  it('detects text', async () => {
-    //...
-  });
-
-  it('allows tapping buttons', async () => {
-    //...
-  });
-
-  it('allows typing into text fields', async () => {
-    //...
-  });
 });
